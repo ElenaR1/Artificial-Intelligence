82c864d79ce93b8b928b5d4252c2e50945e5b95b
ElenaR1 <elena.radoycheva@abv.bg>
2018-12-08 10:47:59 +0000

    Delete Record.java

--- KNN/Record.java
+++
@@ -1,9 +0,0 @@
-public class Record {
-	double[] attributes;
-	int classLabel;
-	
-	Record(double[] attributes, int classLabel){
-		this.attributes = attributes;
-		this.classLabel = classLabel;
-	}
-}
